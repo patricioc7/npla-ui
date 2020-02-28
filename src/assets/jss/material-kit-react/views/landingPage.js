1e684cdf132053bd3b6e492f88d0efb310ee3e52
pcordoba <pato@grayshirts.com>
2020-02-28 16:00:01 -0300

    trying to fix mobile view

--- src/assets/jss/material-kit-react/views/landingPage.js
+++ src/assets/jss/material-kit-react/views/landingPage.js
@@ -27,8 +27,9 @@ const landingPageStyle = {
     zIndex: "3"
   },
   mainRaised: {
-    margin: "-60px 140px 0px",
+    margin: "-60px auto",
     borderRadius: "6px",
+    maxWidth: "80%",
     boxShadow:
       "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
   },
